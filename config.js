49ed52a9e346d81705424751b48546bd434bfb87
Michał Kuliś <michalkulis1993@gmail.com>
2018-08-14 21:04:27 +0200

    Add config.js file

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports= {
+    'GOOGLE_CLIENT_ID': '654508534291-bskd0nt8r8l5ekdoo47e94g88a12fe6v.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'X0pHiZm-3ai1Q8C1mPgmm1Ks',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
